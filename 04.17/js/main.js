e72f3c3d7e7569ad76e8742653c911fed851e28a
AmirAvner <amiravner@gmail.com>
2023-04-27 12:59:31 +0300

    tax excercise code

---
+++ 04.17/js/main.js
@@ -0,0 +1,22 @@
+/*var fname = "amir avner";
+var age = 36;
+var message = "hi, my name is " + fname + " and my age is " + age;
+alert(message);*/
+
+/*var name = "Amir";
+var age = 36;
+var x = "Hi my name is " + name + " and i'm " + age + " years old";
+alert(x);*/
+
+var num = prompt("תכניס מספר");
+num = parseFloat(num);
+if (num){
+    num += 10;
+    alert(num);
+}
+else{
+    alert("טעות")
+}
+
+
+
